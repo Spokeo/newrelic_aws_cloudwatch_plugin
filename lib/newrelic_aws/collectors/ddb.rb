74a30162b55cd4c1a912c2b5e54238d307615aaf
Eric Liang <eric@spokeo.com>
2016-02-19 09:24:40 -0800

    Update to AWS SDK v2.  Query GSI metrics in DynamoDB.

--- lib/newrelic_aws/collectors/ddb.rb
+++ lib/newrelic_aws/collectors/ddb.rb
@@ -1,24 +1,29 @@
+require 'pry'
 module NewRelicAWS
   module Collectors
     class DDB < Base
       def initialize(access_key, secret_key, region, options)
         super(access_key, secret_key, region, options)
         @tables = options[:tables]
+        @gsis = {}
       end
 
       def tables
         return @tables if @tables
-        ddb = AWS::DynamoDB.new(
+        ddb = Aws::DynamoDB::Resource.new(
           :access_key_id => @aws_access_key,
           :secret_access_key => @aws_secret_key,
           :region => @aws_region
         )
-        ddb.tables.map { |table| table.name }
+        ddb.tables.map { |table| 
+          @gsis[table.name] = table.global_secondary_indexes.map { |gsi| gsi.index_name } if table.global_secondary_indexes
+          table.name 
+        }
       end
 
       def metric_list
         [
-          {:metric_name => "ThrottledRequests", :statistic => "Sum", :unit => "Count", :default_value => 0, :dimensions => [["Operation", "PutItem"], ["Operation", "DeleteItem"], ["Operation", "UpdateItem"], ["Operation", "GetItem"], ["Operation", "BatchGetItem"], ["Operation", "BatchWriteItem"], ["Operation", "Scan"], ["Operation", "Query"]]},
+          {:metric_name => "ThrottledRequests", :statistic => "Sum", :unit => "Count", :default_value => 0, :dimensions => [["Operation", "PutItem"], ["Operation", "DeleteItem"], ["Operation", "UpdateItem"], ["Operation", "GetItem"], ["Operation", "BatchGetItem"], ["Operation", "BatchWriteItem"], ["Operation", "Scan"], ["Operation", "Query"]], :ignore_gsi => true},
           {:metric_name => "ProvisionedReadCapacityUnits", :statistic => "Sum", :unit => "Count", :period => 300, :reporting_prefix => "Capacity/Read"},
           {:metric_name => "ProvisionedWriteCapacityUnits", :statistic => "Sum", :unit => "Count", :period => 300, :reporting_prefix => "Capacity/Write"},
           {:metric_name => "ConsumedReadCapacityUnits", :statistic => "Sum", :unit => "Count", :scale => 60, :reporting_prefix => "Capacity/Read"},
@@ -46,6 +51,24 @@ module NewRelicAWS
             else
               metric_dimensions = [[{:name => "TableName", :value => table_name}]]
             end
+
+            # If we have GSIs, we want to get data points for each of the same
+            # metrics of the table itself, except for any metrics with the
+            # :ignore_gsi flag.
+            gsi_dimensions = []
+            for dimension in metric_dimensions
+              break if metric[:ignore_gsi]
+
+              # add a dimension for each existing dimension for each GSI
+              break unless @gsis[table_name]
+              for gsi in @gsis[table_name]
+                gsi_dimension = dimension.dup
+                gsi_dimension << {:name => "GlobalSecondaryIndexName", :value => gsi}
+                gsi_dimensions << gsi_dimension
+              end
+            end
+            metric_dimensions += gsi_dimensions
+
             for dimension in metric_dimensions
               data_point = get_data_point(
                 :namespace      => "AWS/DynamoDB",
@@ -60,7 +83,13 @@ module NewRelicAWS
               )
               unless data_point.nil?
                 data_point[1] = metric[:reporting_prefix] + "/" + data_point[1] if metric[:reporting_prefix]
-                data_point[1] += "/" + dimension.last[:value] if dimension.size > 1
+                if dimension.size > 1
+                  if (gsi_dimension = dimension.select { |h| h[:name] == "GlobalSecondaryIndexName" }).size > 0
+                    data_point[1] = "GSI/" + data_point[1] + "/" + gsi_dimension.first[:value]
+                  else
+                    data_point[1] += "/" + dimension.last[:value]
+                  end
+                end
                 data_point[3] /= metric[:scale] if metric[:scale]
               end
               NewRelic::PlatformLogger.debug("metric_name: #{metric[:metric_name]}, statistic: #{metric[:statistic]}, unit: #{metric[:unit]}, dimension: #{dimension.inspect}, response: #{data_point.inspect}")
@@ -73,11 +102,24 @@ module NewRelicAWS
           # calculate derived metrics
           derived_metrics.each { |attrs|
             if attrs[1] == :ratio
-              # find the required attributes
-              data_point1 = table_data_points.select { |p| p[1].match(attrs[2]) }.first
-              data_point2 = table_data_points.select { |p| p[1].match(attrs[3]) }.first
-              next unless data_point1 && data_point2
-              table_data_points << [table_name, attrs[0], "Percent", data_point1[3] * 100.0 / data_point2[3]]
+              # find all relevant data points
+              numerator_data_points = table_data_points.select { |p| p[1].match(attrs[2]) }
+              denominator_data_points = table_data_points.select { |p| p[1].match(attrs[3]) }
+
+              # for each denominator, find the matching numerator metric
+              for denominator in denominator_data_points
+                if denominator[1].match("GSI/")
+                  gsi_name = denominator[1].split('/').last
+                  numerator = numerator_data_points.select { |p| p[1].match(gsi_name) }.first
+                  metric_name = "GSI/#{attrs[0]}/#{gsi_name}"
+                else
+                  numerator = numerator_data_points.select { |p| !p[1].match("GSI/") }.first
+                  metric_name = attrs[0]
+                end
+
+                next unless numerator
+                table_data_points << [table_name, metric_name, "Percent", numerator[3] * 100.0 / denominator[3]]
+              end
             end
           }
           data_points += table_data_points
